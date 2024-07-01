62ead63aa1a58637010cec78b125a3098e7f5ef1
BelalBasal <basdibrahim@gmail.com>
2024-07-01 22:37:45 +0300

    Add project files.

---
+++ Assignment02D03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment02D03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
