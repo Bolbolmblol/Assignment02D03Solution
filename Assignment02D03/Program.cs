397d6df779d7d08464d3241a24b4c2ac78d01979
BelalBasal <basdibrahim@gmail.com>
2024-07-02 00:15:20 +0300

    Q13

--- Assignment02D03/Program.cs
+++ Assignment02D03/Program.cs
@@ -87,31 +87,31 @@ namespace Assignment02D03
             #region Q6
             /*Write a program that allows the user to insert an integer then print all numbers between 1 to that number.*/
             Console.WriteLine("Enter your number");
-                uint.TryParse(Console.ReadLine() , out uint number7);
-                for (int i = 1; i <=number7; i++)
-                {
-                    Console.WriteLine(i);
-                }
+            uint.TryParse(Console.ReadLine(), out uint number7);
+            for (int i = 1; i <= number7; i++)
+            {
+                Console.WriteLine(i);
+            }
             #endregion
 
             #region Q7
             /*Write a program that allows the user to insert an integer then 
             print a multiplication table up to 12.*/
             Console.WriteLine("Enter your number");
-            int.TryParse(Console.ReadLine() , out int number8);
-            for(int i = 1;i<=12;i++)
+            int.TryParse(Console.ReadLine(), out int number8);
+            for (int i = 1; i <= 12; i++)
             {
-                Console.WriteLine($"{number8}*{i}={number8*i}");
+                Console.WriteLine($"{number8}*{i}={number8 * i}");
             }
             #endregion
 
             #region Q8
             /*Write a program that allows to user to insert number then print all even numbers between 1 to this number*/
             Console.WriteLine("ENTER YOR NUMBER");
-            int.TryParse(Console.ReadLine() , out int number9);
+            int.TryParse(Console.ReadLine(), out int number9);
             for (int i = 1; i <= number9; i++)
             {
-                if(i%2==0)
+                if (i % 2 == 0)
                     Console.WriteLine(i);
             }
             #endregion
@@ -121,9 +121,9 @@ namespace Assignment02D03
             Console.WriteLine("enter number");
             int.TryParse(Console.ReadLine(), out int number10);
             Console.WriteLine("enter power");
-            int.TryParse (Console.ReadLine() , out int power);
+            int.TryParse(Console.ReadLine(), out int power);
             int equal = 1;
-            for (int i = 1;i<=power;i++)
+            for (int i = 1; i <= power; i++)
             {
                 equal *= number10;
             }
@@ -133,13 +133,13 @@ namespace Assignment02D03
             #region Q10
             /*Write a program to enter marks of five subjects and calculate total, average and percentage.*/
             Console.WriteLine("enter  your five degrees");
-            int.TryParse(Console.ReadLine(),out int degree1);
+            int.TryParse(Console.ReadLine(), out int degree1);
             int.TryParse(Console.ReadLine(), out int degree2);
             int.TryParse(Console.ReadLine(), out int degree3);
             int.TryParse(Console.ReadLine(), out int degree4);
             int.TryParse(Console.ReadLine(), out int degree5);
-            int sum=degree1 + degree2+ degree3+degree4+degree5;
-            int average=sum/5;
+            int sum = degree1 + degree2 + degree3 + degree4 + degree5;
+            int average = sum / 5;
             int precentage = sum / 500 * 100;
             Console.WriteLine($"sum is :  {sum} , average is : {average}  , precentage is:  {precentage}");
 
@@ -149,11 +149,11 @@ namespace Assignment02D03
             /*Write a program to input the month number and print the number of days in that month.*/
             Console.WriteLine("enter number month ");
             int.TryParse(Console.ReadLine(), out int month);
-            switch(month)
-            { 
+            switch (month)
+            {
                 case 1:
                 case 3:
-                case 5: 
+                case 5:
                 case 7:
                 case 9:
                 case 10:
@@ -182,10 +182,10 @@ namespace Assignment02D03
             double.TryParse(Console.ReadLine(), out double calcu2);
             Console.WriteLine("enter your operator 1=+ 2=- 3= * 4 = /");
             int.TryParse(Console.ReadLine(), out int operat);
-            switch(operat)
+            switch (operat)
             {
                 case 1:
-                    Console.WriteLine($"sum is {calcu1+calcu2}");
+                    Console.WriteLine($"sum is {calcu1 + calcu2}");
                     break;
                 case 2:
                     Console.WriteLine($"intersect is {calcu1 - calcu2}");
@@ -205,8 +205,19 @@ namespace Assignment02D03
                     break;
             }
 
-
             #endregion
+
+            #region Q13
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+            string reversedString = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversedString += input[i];
+            }
+            Console.WriteLine("Reversed string: " + reversedString);
+            #endregion
+
         }
     }
 }
